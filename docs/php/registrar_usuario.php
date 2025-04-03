026c9d096dd3f142bcda962d55b99692005465d5
Valentina Higuita <valenhiguita67@gmail.com>
2025-04-03 13:33:03 -0500

    Modificaciones

--- docs/php/registrar_usuario.php
+++ docs/php/registrar_usuario.php
@@ -3,7 +3,7 @@ include 'conexion.php';
 
 // Verifica que se envió por POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre = $_POST["name"];
+    $nombre = $_POST['nombre'];
     $email = $_POST["email"];
     $password = password_hash($_POST["pass"], PASSWORD_DEFAULT); // encriptar la contraseña
     $ciudad = $_POST["ciudad"];
